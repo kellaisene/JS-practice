01434c1646ccfeae80a93713d3fc559f365930af
kellerjohnson44 <keller@jjumpp.com>
2019-04-15 15:30:58 -0600

    Formatting

--- rotateImage.js
+++ rotateImage.js
@@ -3,9 +3,7 @@
 // You are given an n x n 2D matrix that represents an image. Rotate the image by 90 degrees (clockwise).
 
 // Example
-
 // For
-
 // a = [[1, 2, 3],
 //      [4, 5, 6],
 //      [7, 8, 9]]
