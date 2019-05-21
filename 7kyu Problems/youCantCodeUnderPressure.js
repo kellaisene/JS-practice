b913181ad94107ec0420bf3e900ac1003498a88e
Keller Johnson <kjlt4412@gmail.com>
2019-05-21 11:18:21 -0600

    New problems

--- 7kyu Problems/youCantCodeUnderPressure.js
+++ 7kyu Problems/youCantCodeUnderPressure.js
@@ -13,3 +13,31 @@ class Counter {
 		return this.count;
 	}
 }
+
+function Counter() {
+	var counter = 0;
+	this.check = function() {
+		return counter;
+	};
+	this.increment = function() {
+		counter++;
+	};
+}
+
+function Counter() {
+	var counter = 0;
+	this.check = () => counter;
+	this.increment = () => counter++;
+}
+
+function Counter() {
+	var i = 0;
+
+	//quickly, now!
+	this.check = () => {
+		return i;
+	};
+	this.increment = () => {
+		i++;
+	}; //
+}
