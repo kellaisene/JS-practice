67e7d4cdf79525b7d23d57f7bcf90153717cb21e
Keller Johnson <kjlt4412@gmail.com>
2019-05-30 09:46:29 -0600

    New hackerrank node problem movieTitles

---
+++ HackerRank/movieTitles.js
@@ -0,0 +1,44 @@
+const https = require('https');
+/*
+ * Complete the function below.
+ * Use console.log to print the result, you should not return from the function.
+ * Base url: https://jsonmock.hackerrank.com/api/movies/search/?Title=
+ */
+function getMovieTitles(substr) {
+	pageNum = 1;
+	let url = 'https://jsonmock.hackerrank.com/api/movies/search/?Title=' + substr;
+	https.get(url, (res) => {
+		res.on('data', function(body) {
+			let dataRec = JSON.parse(body);
+			let movies = dataRec.data;
+			let totPages = dataRec.total_pages;
+			let sortArray = [];
+			movies.map((a) => {
+				sortArray.push(a.Title);
+			});
+			if (totPages > 1) {
+				for (let i = 2; i <= totPages; i++) {
+					let newPage = i;
+					let url1 =
+						'https://jsonmock.hackerrank.com/api/movies/search/?Title=' + substr + '&page=' + newPage;
+					https.get(url1, (res) => {
+						res.on('data', function(body) {
+							let newData = JSON.parse(body);
+							let newMovies = newData.data;
+							for (let i = 0; i < newMovies.length; i++) {
+								sortArray.push(newMovies[i].Title);
+							}
+							let sorted = sortArray.sort();
+							let final = sorted.join('\n');
+							console.log('final', final);
+							return final;
+						});
+					});
+				}
+			} else {
+				let sorted = sortArray.sort();
+				return sorted;
+			}
+		});
+	});
+}
